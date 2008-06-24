dc872b864be85f6c8366ecd61dc044a8a75a7fc9
Wil Gieseler <supapuerco@gmail.com>
2008-06-24 10:38:52 +0000

    highly shortened and cleaned up parser

--- XBGamesPlayedParser.m
+++ XBGamesPlayedParser.m
@@ -13,201 +13,40 @@ NSString* achievementURL = @"http://live.xbox.com/en-US/profile/Achievements/Vie
 
 @implementation XBGamesPlayedParser
 
-- (id)init	{
-	if (![super init])
-	return nil;
-
-	return self;
++ (NSArray *)fetchWithTag:(NSString *)tag {
+	return [self fetchWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@", achievementURL, [tag replace:@" " with:@"+"]]]];
 }
 
-+ (NSArray *)fetchWithTag:(NSString *)tag
-{
-	NSMutableString *mutableGamerTag = [[tag mutableCopy] autorelease];
-	[mutableGamerTag replaceOccurrencesOfString:@" " withString:@"+" options:0 range:NSMakeRange(0, [mutableGamerTag length])];
-	return [self fetchWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@", achievementURL, mutableGamerTag]]];
-}
-
-+ (NSArray *)fetchWithURL:(NSURL *)URL
-{	
++ (NSArray *)fetchWithURL:(NSURL *)URL {	
 
 	NSString *editString = [NSString stringWithContentsOfURL:URL encoding:NSUTF8StringEncoding error:nil];
-	NSMutableArray *rows = [[NSMutableArray alloc] init];
-
-	//rows
-	while ([editString rangeOfString:@"<tbody"].location != NSNotFound ) {
-	
-		NSRange range;
-		int offset;
-		range = [editString rangeOfString:@"<tbody"];
-		offset = range.location + range.length;
-		range = [editString rangeOfString:@"</tr>" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-		NSString *thisRow = [editString substringWithRange:NSMakeRange(offset, range.location - offset)];
-		[rows addObject:thisRow];
-				
-		editString = [editString substringFromIndex:range.location];
-	}
+	NSArray *rows = [editString cropRowsMatching:@"<tbody" rowEnd:@"</tr>"];
 
 	NSMutableArray *gamesArray = [NSMutableArray array];
-	NSEnumerator *enumerator = [rows objectEnumerator];
-	id rowSource;
-	while ((rowSource = [enumerator nextObject])) {
+
+	for (NSString *row in rows) {
 		
 		XBGame *thisGame = [XBGame game];
-		[thisGame setName:[MQFunctions cropString:rowSource between:@"XbcAchievementsTitle\">" and:@"</strong>"]];
-		[thisGame setTileURL:[NSURL URLWithString:[MQFunctions cropString:rowSource between:@"AchievementsGameIcon\" src=\"" and:@"\" alt=\""]]];
+		[thisGame setName:[row cropFrom:@"XbcAchievementsTitle\">" to:@"</strong>"]];
+		[thisGame setTileURL:[NSURL URLWithString:[row cropFrom:@"AchievementsGameIcon\" src=\"" to:@"\" alt=\""]]];
 
-		NSString *yourScore = [MQFunctions cropString:rowSource between:@"XbcAchMeCell\"><strong>" and:@" <img src"];
+		NSString *yourScore = [row cropFrom:@"XbcAchMeCell\"><strong>" to:@" <img src"];
 		if ([yourScore rangeOfString:@"No Gamerscore"].location != NSNotFound || yourScore == nil)
 			yourScore = @"-1 of 1000";
 		[thisGame setYourScore:yourScore];
 
-		NSString *theirScore = [MQFunctions cropString:rowSource between:@"XbcAchYouCell\"><strong>" and:@" <img src"];
+		NSString *theirScore = [row cropFrom:@"XbcAchYouCell\"><strong>" to:@" <img src"];
 		if ([theirScore rangeOfString:@"No Gamerscore"].location != NSNotFound || theirScore == nil)
 			theirScore = @"-1 of 1000";
 		[thisGame setTheirScore:theirScore];
 
-		[thisGame setGameID:[MQFunctions cropString:rowSource between:@"ViewAchievementDetails.aspx?tid=" and:@"&amp;"]];
+		[thisGame setGameID:[row cropFrom:@"ViewAchievementDetails.aspx?tid=" to:@"&amp;"]];
 
 		[gamesArray addObject:thisGame];
 	}
 
-	[rows release];
 	return [[gamesArray copy] retain];
 }
-
-//+ (NSMutableDictionary *)fetchWithTag:(NSString *)tag
-//{
-//	NSMutableString *mutableGamerTag = [[tag mutableCopy] autorelease];
-//	[mutableGamerTag replaceOccurrencesOfString:@" " withString:@"+" options:0 range:NSMakeRange(0, [mutableGamerTag length])];
-//	return [self fetchWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@", achievementURL, mutableGamerTag]]];
-//}
-
-//
-//+ (NSMutableDictionary *)fetchWithURL:(NSURL *)URL
-//{
-//	//make dictionary
-//	NSMutableDictionary *achievementDict = [NSMutableDictionary dictionary];
-//
-//	//make arrays
-//	NSMutableArray *yourScores = [[NSMutableArray alloc] init];
-//	NSMutableArray *theirScores = [[NSMutableArray alloc] init];
-//	//NSMutableArray *yourAchievementCount = [[NSMutableArray alloc] init];
-//	//NSMutableArray *theirAchievementCount = [[NSMutableArray alloc] init];
-//	NSMutableArray *gameNames = [[NSMutableArray alloc] init];
-//	NSMutableArray *gameIcons = [[NSMutableArray alloc] init];
-//	NSMutableArray *gameIDs = [[NSMutableArray alloc] init];
-//
-//	NSLog(@"about to download source");
-//
-//	//download source
-//	NSString *string = [NSString stringWithContentsOfURL:URL encoding:NSUTF8StringEncoding error:nil];//[NSString stringWithContentsOfURL:URL encod];
-//	NSString *editString = string;
-//		NSLog(@"source done");
-//
-//	
-//	//gameNames
-//	while ([editString rangeOfString:@"XbcAchievementsTitle\">"].location != NSNotFound )	{
-//		NSRange range;
-//		int offset;
-//
-//		range = [editString rangeOfString:@"XbcAchievementsTitle\">"];
-//		offset = range.location + range.length;
-//
-//		range = [editString rangeOfString:@"</strong>" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-//
-//		NSString *thisGameName = [editString substringWithRange:NSMakeRange(offset, range.location - offset)];
-//
-//		[gameNames addObject:thisGameName];
-//				
-//		editString = [editString substringFromIndex:range.location];
-//	}
-//	[achievementDict setObject:gameNames forKey:@"gameNames"];
-//
-//	//gameIDs
-//	editString = string;
-//	while ([editString rangeOfString:@"ViewAchievementDetails.aspx?tid="].location != NSNotFound )	{
-//	
-//		NSRange range;
-//		int offset;
-//
-//		range = [editString rangeOfString:@"ViewAchievementDetails.aspx?tid="];
-//		offset = range.location + range.length;
-//		range = [editString rangeOfString:@"&amp;" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-//
-//		[gameIDs addObject:[MQFunctions cropString:editString between:@"ViewAchievementDetails.aspx?tid=" and:@"&amp;"]];
-//
-//		editString = [editString substringFromIndex:range.location];
-//	}
-//	[achievementDict setObject:gameIDs forKey:@"gameIDs"];
-//
-//	//gameIcons
-//	editString = string;
-//	while ([editString rangeOfString:@"AchievementsGameIcon\" src=\""].location != NSNotFound )	{
-//		NSRange range;
-//		int offset;
-//
-//		range = [editString rangeOfString:@"AchievementsGameIcon\" src=\""];
-//		offset = range.location + range.length;
-//
-//		range = [editString rangeOfString:@"\" alt=\"" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-//
-//		NSString *thisGameIcon = [editString substringWithRange:NSMakeRange(offset, range.location - offset)];
-//
-//		[gameIcons addObject:thisGameIcon];
-//				
-//		editString = [editString substringFromIndex:range.location];
-//	}
-//	[achievementDict setObject:gameIcons forKey:@"gameIcons"];
-//
-//	//yourScores
-//	editString = string;
-//	while ([editString rangeOfString:@"XbcAchMeCell\"><strong>"].location != NSNotFound )	{
-//		NSRange range;
-//		int offset;
-//
-//		range = [editString rangeOfString:@"XbcAchMeCell\"><strong>"];
-//		offset = range.location + range.length;
-//
-//		range = [editString rangeOfString:@"</strong>" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-//
-//		NSString *ys = [editString substringWithRange:NSMakeRange(offset, range.location - offset)];
-//
-//		if ([ys rangeOfString:@"No Gamerscore"].location != NSNotFound){
-//			ys = @"-1 of 0";
-//		}
-//
-//		[yourScores addObject:[MQFunctions flattenHTML:ys]];
-//
-//		editString = [editString substringFromIndex:range.location];
-//	}
-//	[achievementDict setObject:yourScores forKey:@"yourScores"];
-//
-//	//theirScores
-//	editString = string;
-//	while ([editString rangeOfString:@"XbcAchYouCell\"><strong>"].location != NSNotFound )	{
-//		NSRange range;
-//		int offset;
-//
-//		range = [editString rangeOfString:@"XbcAchYouCell\"><strong>"];
-//		offset = range.location + range.length;
-//
-//		range = [editString rangeOfString:@"</strong>" options:0 range:NSMakeRange(offset, [editString length] - offset)];
-//
-//		NSString *ts = [editString substringWithRange:NSMakeRange(offset, range.location - offset)];
-//		if ([ts rangeOfString:@"No Gamerscore"].location != NSNotFound){
-//			ts = @"0 of 0";
-//		}
-//
-//		[theirScores addObject:[MQFunctions flattenHTML:ts]];
-//				
-//		editString = [editString substringFromIndex:range.location];
-//	}
-//	[achievementDict setObject:theirScores forKey:@"theirScores"];
-//
-//
-//	return achievementDict;
-//}
-//
 
 
 @end
